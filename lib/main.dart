83fbeaf1f1d80564d24dc58c1de988aebf8bffda
26530 <2653019235@qq.com>
2024-06-06 22:02:05 +0800

    moldify

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title:'Flutter Demo',
+      theme: ThemeData(
+        colorScheme:ColorScheme.fromSeed(seedColor:Colors.deepPurple),
+        useMaterial3:true,
       ),
+      home: const MyHomePage(title:'web of ZiRui'),
     );
   }
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key,required this.title});
+  final String title;
+  State<MyHomePage> createState()=>_MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  
+  int _counter = 0;
+
+  void _incrementCounter() {
+  setState((){
+    _counter++;
+  });
+}
+
+Widget build(BuildContext contest){
+  return Scaffold(appBar:AppBar(backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  title:Text(widget.title),),
+  body:Center(child:Column(mainAxisAlignment:MainAxisAlignment.center,children: <Widget>[const Text('You have pushed button this many times:',),
+  Text('$_counter',
+  style:Theme.of(context).textTheme.headlineMedium,),],),),
+  floatingActionButton: FloatingActionButton(
+    onPressed:_incrementCounter,
+    tooltip:'Increment',
+    child:const Icon(Icons.add),
+  ),
+  );
+}
 }
